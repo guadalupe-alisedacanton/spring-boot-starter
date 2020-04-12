4909ee9a008006acbcb4dda127d327cd2ba2967f
s-alisedag <s-alisedag@53S50029749.it.bsd405.org>
2020-04-12 12:28:17 -0700

    Initial setup of classes and nowPlaying html template.

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,23 +1,64 @@
 package com.example.demo;
 
+import java.util.List;
+
+import org.json.JSONException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
-	@GetMapping("/greeting")
+	@Autowired
+	private Main mainCode;
+	
+	/*@GetMapping("/greeting")
 	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
 		return "greeting";
 	}
+	*/
+//	@GetMapping("/")
+//	public String intro() {
+//		Main main = new Main();
+//		Controller control = new Controller(main);
+//		List<Movie> nowPlaying = main.setupNowPlayingList();
+//		return "greeting";	
+//	}
 	
-	@GetMapping("/movies")
-	public String movies(@RequestParam(name="name", required=false, defaultValue="Movies List") String name, Model model) {
-		model.addAttribute("name", name);
-		return "movies";
+	@GetMapping("/nowPlaying")
+	public String fetchNowPlayingList(Model model) {
+		List<Movie> result = null;
+		
+		
+		try {
+			
+			result = mainCode.setupNowPlayingList();
+			
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//System.out.println("This is result" + result);
+		model.addAttribute("movies", result);
+		
+		return "nowPlaying";
 	}
+	
+	@GetMapping("/upcoming")
+	public String fetchUpcomingList() {
+		List<Movie> result = null;
+		try {
+			result =  mainCode.setupUpcomingList();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "greeting";
+	}
+	
+	
 
 }
